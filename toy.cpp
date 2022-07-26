ff2fab1fd4462bed1e4b2f2b6983de9de02b1782
lottaquestions <nickothieno@yahoo.com>
2022-07-26 15:58:19 -0400

    Added documentation about tests run and extra packages that are needed in Ubuntu

--- toy.cpp
+++ toy.cpp
@@ -10,6 +10,25 @@
 // To compile:
 // clang++ -stdlib=libc++ -g -O3 toy.cpp `llvm-config --cxxflags` -o toy.bin
 
+// Extra package that was needed to be installed
+// sudo apt-get install libc++abi-dev
+
+
+//  Tests to run in the kaliedoscop REPL:
+//  ~/work/compiler_design/Kaleidoscope$ ./toy.bin 
+//  ready> def foo(x y) x+foo(y, 4.0);
+//  ready> Parsed a function definition.
+//  ready> def foo(x y) x+y y;
+//  ready> Parsed a function definition.
+//  ready> Parsed a top-level expression.
+//  ready> def foo(x y) x+y );
+//  ready> Parsed a function definition.
+//  ready> Error: unknown token when expecting an expression
+//  ready> extern sin(a);
+//  ready> Parsed an extern.
+//  ready> 
+ 
+
 
 //=============================================
 // Lexer
